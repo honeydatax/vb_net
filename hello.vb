cba11b9975fccd0f30ed7618bcf8cd73f1bffd9f
lisbon <16432726+honeydatax@users.noreply.github.com>
2018-11-02 23:31:07 +0000

    hello world visual basic net

---
+++ hello.vb
@@ -0,0 +1,10 @@
+Imports System
+
+'http://www.compileonline.com/compile_vb.net_online.php
+Module Module1
+ 
+   Sub Main()
+      Console.WriteLine("planet source code")
+      Console.ReadKey()
+   End Sub
+End Module
